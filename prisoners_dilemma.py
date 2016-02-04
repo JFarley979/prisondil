e4146afc013b075df9d2ad91cad2b547705296a3
JFarley979 <jfarley-student@nfcs.k12.pa.us>
2016-02-04 10:59:09 -0500

    Change 1
    We changed our team name, and we changed the purpose of the code.

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -21,7 +21,7 @@ CSE Project 1.3.5 Collaborating on a Project
 Draft, Do Not Distribute
 Version 8/23/2013 
 '''
-
+ 
 import random
 def play_round(player1, player2, history1, history2, score1, score2):
     '''
@@ -348,16 +348,14 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #
     elif player == 9:
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'The Ducklings'
         else:
             # use history, opponent_history, score, opponent_score
             # to compute your strategy
             if len(opponent_history)==0: #It's the first round: collude
                 return 'c'
             elif history[-1]=='c' and opponent_history[-1]=='b':
-                return 'b' # betray is they were severely punished last time
-            else:
-                return 'c' #otherwise collude
+                return 'b' #otherwise collude
 
 
 
